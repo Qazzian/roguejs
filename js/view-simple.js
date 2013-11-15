617f6f556c50127165d400774ff65e4d0fc1ef2b
Ian Wallis <ian@travelmatch.co.uk>
2013-11-15 10:15:34 +0000

    Refactor the simple web view

--- js/view-simple.js
+++ js/view-simple.js
@@ -1,48 +1,70 @@
+(function(){
 
+"use strict";
 
-function SimpleView(map){
+window.SimpleView = function (map){
 	this.map = map;
-}
+};
 
 SimpleView.prototype = {
 	ready: false,
+	container: null,
+	tbody: null,
+	grid: null, // 2d Array[w][h] which point to the html elements that represent the corrisponding map posiitons.
 
 	init: function(options){
 		this.container = $('#dungeon-view');
-	},
-
-	print: function(){
 		this.container.html('');
+
 		var map = this.map;
 		var table = $(document.createElement('table'));
-		var tbody = $(document.createElement('tbody'));
-		table.append(tbody);
+		this.tbody = $(document.createElement('tbody'));
+		table.append(this.tbody);
 
+		this.grid = [];
+		for (var i=0; i<map.width; i++) {
+			this.grid[i] = [];
+		}
 
 		for (var y=0; y<map.height; y++) {
 			var row = $(document.createElement('tr'));
-			tbody.append(row);
+			this.tbody.append(row);
 			for (var x=0; x<map.width; x++) {
-				var td = $(document.createElement('td'));
+				var td = document.createElement('td');
 				row.append(td);
-				var obj = this.map.getObject(x, y);
-				if (obj) {
-					td.html(obj.icon);
-					if (obj.color) {
-						td.css({color: obj.color});
-					}
-				}
-				else {
-					td.html(' ');
-				}
-				
-				
+				this.grid[x][y] = td;
+
+				this.printTile(x, y);
+
 				
 			}
 		}
 		this.container.append(table);
+
+	},
+
+
+	print: function(){
+		for (var x=0; x<this.map.width; x++) {
+			for (var y=0; y<this.map.height; y++) {
+				this.printTile(x, y);
+			}
+		}
+	},
+	printTile: function(x, y){
+		var td = this.grid[x][y],
+			tile = this.map.getTile(x, y),
+			obj = this.map.getObject(x, y);
+
+		this.printIcon(td, obj || tile);
+	},
+
+	printIcon: function(elmt, gameObj){
+		elmt.innerHTML = gameObj.icon || ' ';
+		// elmt.css({color: gameObj.color || ''});
 	}
 };
 
 
 
+})();
